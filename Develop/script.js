b19c6ca8e7bc82c1b24cd56d139ad43f80cf0d71
Claire Phillips <claireacam@gmail.com>
2021-02-12 19:07:53 -0500

    finished writing the functions for the password generator.

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,23 @@ let copyButton = document.querySelector("#display");
 let upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+let numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// This check that a number is an integer and not a float
+function isInt(n) {
+  return n % 1 === 0;
+}
 
 // Function for choosing password length
 function chooseLength() {
-  let passwordLength = window.prompt("Pick a password length between 8-128 characters.");
-  let counter = 0; 
-  while( !Number.isInteger(passwordLength) || passwordLength < 8 || passwordLength > 128){
-    counter++; 
+  let passwordLength = +window.prompt("Pick a password length between 8-128 characters.");
+  let counter = 0;
+  while (isNaN(passwordLength) || !isInt(passwordLength) || passwordLength < 8 || passwordLength > 128){
+    counter++;
     if( counter < 3 ){
-      passwordLength = prompt("Password length must be a number between 8-128 characters.");
+      passwordLength = +prompt("Password length must be a number between 8-128 characters.");
     } else {
-      passwordLength = prompt("PASSWORD LENGTH MUST BE A NUMBER BETWEEN 8-128 CHARACTERS!!!!");
+      passwordLength = +prompt("PASSWORD LENGTH MUST BE A NUMBER BETWEEN 8-128 CHARACTERS!!!!");
     }
   }
   return passwordLength;
@@ -44,22 +50,42 @@ function numbersPrompt() {
 // Function to generate password
 function generatePassword() {
   let passwordLength = chooseLength();
-  let useUpperCase = upperCasePrompt();
-  let useLowerCase = lowerCasePrompt();
-  let useSpecialCharacters = specialCharactersPrompt();
-  let useNumbers = numbersPrompt();
+  let useUpperCase = false;
+  let useLowerCase = false;
+  let useSpecialCharacters = false;
+  let useNumbers = false;
   while (!useUpperCase && !useLowerCase && !useSpecialCharacters && !useNumbers) {
-    window.alert("You must choose at least one type of character to use in your password");
+    window.alert("You must choose at least one of the following types of characters to use in your password.");
     useUpperCase = upperCasePrompt();
     useLowerCase = lowerCasePrompt();
     useSpecialCharacters = specialCharactersPrompt();
     useNumbers = numbersPrompt();
   }
 
-  for (let i = 0; i < passwordLength; i++) {
-   
-    
+  // Making an array based on what the user picked
+  let choices = [];
+  if (useUpperCase) {
+    choices = choices.concat(upperCaseLetters);
   }
+  if (useLowerCase) {
+    choices = choices.concat(lowerCaseLetters);
+  }
+  if (useSpecialCharacters) {
+    choices = choices.concat(specialCharacters);
+  }
+  if (useNumbers) {
+    choices = choices.concat(numberCharacters);
+  }
+
+  // Generate password of appropriate length using choices
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let index = Math.floor(Math.random() * choices.length);
+    let choice = choices[index];
+    password += choice;
+  }
+
+  return password;
 }
 
 // Write password to the #password input
@@ -75,10 +101,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
  
 
-// function to copy password to clipboard
+// Function to copy password to clipboard
 function copyPassword(){
 
-  document.getElementById("display").select();
+  document.getElementById("password").select();
 
   document.execCommand("Copy");
 
